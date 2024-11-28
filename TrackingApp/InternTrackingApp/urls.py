bdae216e29695e61cfdb08e8bdc3499aef63fcb6
Riecky31 <127891943+Riecky31@users.noreply.github.com>
2024-11-29 00:48:58 +0200

    commit

--- TrackingApp/InternTrackingApp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import SalesforceAccountList
-
-urlpatterns = [
-    path('salesforce/contacts/', SalesforceAccountList.as_view(), name='salesforce-contacts'),
-]
